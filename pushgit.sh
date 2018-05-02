98c6f5d9ff608fc620fd703c28878c6ebdd98edd
aymen <aymen@localhost.localdomain>
2018-05-02 21:16:45 +0200

    new.project

--- pushgit.sh
+++ pushgit.sh
@@ -1,7 +1,7 @@
 	#!/bin/bash
 	clear
-    git flow feature start new.project
-    git add .
-    git commit -m "new.project"
-    git flow feature finish new.project
-    git push origin develop
+    	git flow feature start new.project
+    	git add .
+    	git commit -m "new.project"
+    	git flow feature finish new.project
+    	git push -f origin develop
